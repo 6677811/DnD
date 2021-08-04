4698a50c64ed9d68e0b52bc49621ca3e61cc7002
6677811 <79306475+6677811@users.noreply.github.com>
2021-08-04 19:11:06 +0300

    feat: add delete figure

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import './Menu.css';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as actions from '../../actions';
 
-const Menu = () => {
-    return(<nav className={'menu'}>
+const Menu = ({selectedFigure, figures, setFigures}) => {
+    const deleteFigure = () => {
+            const newFigures = figures
+                .filter((figure) => figure.id !== selectedFigure.id)
+                .map((figure, idx) => {
+                    figure.id = `added_${idx}`;
+                    return figure;
+                });
+            setFigures(newFigures);
+    };
+
+    return (<nav className={'menu'}>
         <button>Import</button>
         <button>Export</button>
-        <button>Delete</button>
+        <button onClick={deleteFigure}>Delete</button>
     </nav>);
 };
+const mapStateToProps = ({selectedFigure, figures}) => ({selectedFigure, figures});
 
-export default Menu;
+const mapDispatchToProps = (dispatch) => {
+    return bindActionCreators(actions, dispatch);
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Menu);
