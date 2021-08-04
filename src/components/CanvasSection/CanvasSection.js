dc3ff043beede138692f9dfef1a6870b248c4a42
6677811 <79306475+6677811@users.noreply.github.com>
2021-08-04 11:35:46 +0300

    feat: add style for active figure

--- src/components/CanvasSection/CanvasSection.js
+++ src/components/CanvasSection/CanvasSection.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import './CanvasSection.css';
 
+const setActiveFigure = (e) => {
+    e.stopPropagation();
+    document
+        .querySelectorAll('.active-figure')
+        .forEach((figure) => {
+            figure.classList.remove('active-figure');
+        });
+    e.target.classList.add('active-figure');
+    console.log('click');
+}
+
 const CanvasSection = () => {
     const dragOverHandler = (e) => {
         e.preventDefault();
@@ -12,12 +23,10 @@ const CanvasSection = () => {
         const {left, top} = canvas.getBoundingClientRect();
 
         if (data && (data === 'circle' || data === 'rect')) {
-            const canvas = document.querySelector('.canvas__workspace');
             const current = document.querySelector(`#${data}`);
             const nodeCopy = current.cloneNode(true);
-            const target = canvas.getBoundingClientRect();
             nodeCopy.addEventListener('dragstart', dragstartHandler)
-            nodeCopy.classList.add('added-element');
+            nodeCopy.addEventListener('mousedown', setActiveFigure)
             nodeCopy.id = `added_${canvas.childElementCount}`;
             nodeCopy.style.left = e.clientX - left - 73 + 'px';
             nodeCopy.style.top = e.clientY - top - 73 + 'px';
--- src/components/Figure/Figure.js
+++ src/components/Figure/Figure.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import './Figure.css';
 
 const Circle = styled.div`
   width: 100px;
@@ -7,14 +8,14 @@ const Circle = styled.div`
   border-radius: 50%;
   margin: 20px;
   background-color: tomato;
-  border: 3px solid #e3553b`;
-
+  border: 3px solid #d7472d`;
 const Rect = styled.div`
   width: 100px;
   height: 100px;
   margin: 20px;
   background-color: #3f7215;
   border: 3px solid #27480d `;
+
 
 const Figure = (props) => {
     const {isRect, dragstart} = props;
